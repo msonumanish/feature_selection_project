15ab175b73808c3cbf29b44e621d4364e6aa2491
msonumanish <msonu.manish@gmail.com>
2017-10-30 05:53:36 +0000

    Done

--- q03_rf_rfe/build.py
+++ q03_rf_rfe/build.py
@@ -1,3 +1,4 @@
+# %load q03_rf_rfe/build.py
 # Default imports
 import pandas as pd
 
@@ -9,4 +10,17 @@ from sklearn.ensemble import RandomForestClassifier
 
 # Your solution code here
 
+def rf_rfe(df):
+    X = df.iloc[:,:-1]
+    #print(X)
+    Y = df['SalePrice']
+    model = RandomForestClassifier()
+    rfe = RFE(model, n_features_to_select=17)
+    fit = rfe.fit(X, Y)
+    #print("Num Features: %d") % fit.n_features_
+    return X.columns.values[fit.support_].tolist()
+    #print("Selected Features: %s") % fit.support_
+    #print("Feature Ranking: %s") % fit.ranking_
 
+#l=rf_rfe(data)
+#print(type(l))
